db6f73bf43062b2c5295f0d4f67198f995521390
RealIndrit <externalcontact.shield824@passmail.net>
2024-11-23 14:13:20 +0100

    init

---
+++ ccx/CCX.c
@@ -0,0 +1,107 @@
+#include <string.h>
+#include "CCX.h"
+
+CCXContext* CCXInitContext(const char *filePath) {
+    CCXContext *context = (CCXContext *)malloc(sizeof(CCXContext));
+    context->ccxFile = (CCXFile *)malloc(sizeof(CCXFile));
+    context->ccxFile->data = NULL;
+    context->filePath = strdup(filePath);
+    return context;
+}
+
+CCXenum CCXReadFile(CCXContext *context) {
+    if (context == NULL || context->filePath == NULL) {
+        return CCX_INVALID_CONTEXT;
+    }
+
+    FILE *file = fopen(context->filePath, "rb");
+    if (file == NULL) {
+        return CCX_INVALID_CONTEXT;
+    }
+
+    // Read the header
+    if (fread(&context->ccxFile->header, sizeof(CCXFileHeader), 1, file) != 1) {
+        fclose(file);
+        return CCX_INVALID_CONTEXT;
+    }
+
+    // Allocate memory for the data array
+    context->ccxFile->dataCount = context->ccxFile->header.dataLength;
+    context->ccxFile->data = (CCXFileData *)malloc(context->ccxFile->dataCount * sizeof(CCXFileData));
+    if (context->ccxFile->data == NULL) {
+        fclose(file);
+        return CCX_ALLOCATION_FAILURE;
+    }
+
+    // Read the file data
+    for (uint32_t i = 0; i < context->ccxFile->dataCount; i++) {
+        CCXFileData *dataEntry = &context->ccxFile->data[i];
+
+        // Read fixed-size fields
+        fread(&dataEntry->fileNameLength, sizeof(dataEntry->fileNameLength), 1, file);
+        fread(dataEntry->spacer1, sizeof(dataEntry->spacer1), 1, file);
+
+        // Read variable-length file name
+        dataEntry->file = (char *)malloc(dataEntry->fileNameLength + 1);
+        fread(dataEntry->file, dataEntry->fileNameLength, 1, file);
+        dataEntry->file[dataEntry->fileNameLength] = '\0';
+
+        fread(dataEntry->unknown1, sizeof(dataEntry->unknown1), 1, file);
+        fread(dataEntry->CRC1, sizeof(dataEntry->CRC1), 1, file);
+        fread(dataEntry->CRC2, sizeof(dataEntry->CRC2), 1, file);
+        fread(&dataEntry->boolUnkown, sizeof(dataEntry->boolUnkown), 1, file);
+        fread(dataEntry->spacer2, sizeof(dataEntry->spacer2), 1, file);
+        fread(dataEntry->unknown2, sizeof(dataEntry->unknown2), 1, file);
+        fread(&dataEntry->sourceFileNumber, sizeof(dataEntry->sourceFileNumber), 1, file);
+        fread(dataEntry->unknown3, sizeof(dataEntry->unknown3), 1, file);
+        fread(&dataEntry->filePathLength, sizeof(dataEntry->filePathLength), 1, file);
+        fread(dataEntry->spacer3, sizeof(dataEntry->spacer3), 1, file);
+
+        // Read variable-length file path
+        dataEntry->filePath = (char *)malloc(dataEntry->filePathLength + 1);
+        fread(dataEntry->filePath, dataEntry->filePathLength, 1, file);
+        dataEntry->filePath[dataEntry->filePathLength] = '\0';
+    }
+
+    fclose(file);
+    return CCX_NONE;
+}
+
+CCXFileData** CCXGetFiles(CCXContext *context) {
+    return context->ccxFile->data;
+}
+
+CCX_EXPORT uint32_t CCXFindFilesByName(CCXContext *context, const char *filePath, CCXFileData **results) {
+    CCXFileData *tempResults = NULL;
+    uint32_t count = 0;
+
+    for (uint64_t i = 0; i < context->ccxFile->dataCount; ++i) {
+        CCXFileData *data = &context->ccxFile->data[i];
+        if (strstr(data->filePath, filePath) != NULL) {
+            CCXFileData *newResults = realloc(tempResults, (count + 1) * sizeof(CCXFileData));
+            if (newResults == NULL) {
+                free(tempResults);
+                *results = NULL;
+                return 0;
+            }
+            tempResults = newResults;
+            tempResults[count] = *data;
+            count++;
+        }
+    }
+    *results = tempResults;
+    return count;
+}
+
+
+CCXenum CCXFreeContext(CCXContext *context) {
+    for (size_t i = 0; i < context->ccxFile->dataCount; ++i) {
+        free(context->ccxFile->data[i].file);
+        free(context->ccxFile->data[i].filePath);
+    }
+    free(context->ccxFile->data);
+    free(context->ccxFile);
+    free(context->filePath);
+    free(context);
+    return CCX_NONE;
+}
---
+++ ccx/CCX.h
@@ -0,0 +1,181 @@
+#ifndef CCX_H
+#define CCX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef _WIN32
+#define CCX_EXPORT __declspec(dllexport)
+#else
+#define CCX_EXPORT
+#endif
+
+
+/**
+ * @enum CCXenum
+ * @brief Enumeration for CCX error codes.
+ *
+ * This enumeration defines various error codes used in CCX file operations.
+ *
+ * @var CCX_NONE No error.
+ * @var CCX_INVALID_CONTEXT Invalid context error.
+ * @var CCX_ALLOCATION_FAILURE Allocation failure error.
+ * @var CCX_FREE_ALLOCATION_FAILURE Free allocation failure error.
+ */
+typedef enum {
+    CCX_NONE               = 0x0000, ///< No error.
+    CCX_INVALID_CONTEXT    = 0x0001, ///< Invalid context error.
+    CCX_ALLOCATION_FAILURE = 0x0002, ///< Allocation failure error.
+    CCX_FREE_ALLOCATION_FAILURE = 0x0003 ///< Free allocation failure error.
+} CCXenum;
+
+/**
+ * @typedef CCX_SPACER
+ * @brief Spacer for padding/unknown data.
+ *
+ * This type defines a 3-byte spacer used to align data or represent unknown bytes in the structure.
+ */
+typedef uint8_t CCX_SPACER[3];
+
+#pragma pack(push, 1)
+/**
+ * @struct CCXFileHeader
+ * @brief Structure representing the header of a CCX file.
+ *
+ * This structure is used to store the header information for CCX files, which includes unknown data fields and the total length of the data.
+ *
+ * @var unknown Unknown data (5 bytes).
+ * @var spacer1 Unknown data (3 bytes) used as padding or for unknown purposes.
+ * @var dataLength Total length of data (4 bytes).
+ */
+typedef struct {
+    uint8_t unknown[5];    ///< Unknown data (5 bytes).
+    CCX_SPACER spacer1;    ///< Unknown data (3 bytes) used as padding or for unknown purposes.
+    uint32_t dataLength;   ///< Total length of data (4 bytes).
+} CCXFileHeader;
+
+/**
+ * @struct CCXFileData
+ * @brief Structure representing CCX File Data.
+ *
+ * This structure is used to store information related to CCX files, including metadata, CRC checksums, and file paths.
+ *
+ * @var fileNameLength Length of the file name (1 byte).
+ * @var spacer1 Unknown data (3 bytes) used as padding or for unknown purposes.
+ * @var file File name (variable length).
+ * @var unknown1 Unknown data (8 bytes).
+ * @var CRC1 CRC1 checksum (4 bytes).
+ * @var CRC2 CRC2 checksum (4 bytes).
+ * @var boolUnkown Unknown boolean data (1 byte).
+ * @var spacer2 Unknown data (3 bytes) used as padding or for unknown purposes.
+ * @var unknown2 Unknown data (12 bytes).
+ * @var sourceFileNumber Source file number (1 byte).
+ * @var unknown3 Unknown data (7 bytes).
+ * @var filePathLength Length of the file path (1 byte).
+ * @var spacer3 Unknown data (3 bytes) used as padding or for unknown purposes.
+ * @var filePath File path (variable length).
+ */
+typedef struct {
+    uint8_t fileNameLength;    ///< Length of the file name (1 byte).
+    CCX_SPACER spacer1;        ///< Unknown data (3 bytes) used as padding or for unknown purposes.
+    char *file;                ///< File name (variable length).
+    uint8_t unknown1[8];       ///< Unknown data (8 bytes).
+    uint8_t CRC1[4];           ///< CRC1 checksum (4 bytes).
+    uint8_t CRC2[4];           ///< CRC2 checksum (4 bytes).
+    uint8_t boolUnkown;        ///< Unknown boolean data (1 byte).
+    CCX_SPACER spacer2;        ///< Unknown data (3 bytes) used as padding or for unknown purposes.
+    uint8_t unknown2[12];      ///< Unknown data (12 bytes).
+    uint8_t sourceFileNumber;  ///< Source file number (1 byte).
+    uint8_t unknown3[7];       ///< Unknown data (7 bytes).
+    uint8_t filePathLength;    ///< Length of the file path (1 byte).
+    CCX_SPACER spacer3;        ///< Unknown data (3 bytes) used as padding or for unknown purposes.
+    char *filePath;            ///< File path (variable length).
+} CCXFileData;
+
+#pragma pack(pop)
+
+/**
+ * @struct CCXFile
+ * @brief Structure representing the overall CCX file with header and data.
+ *
+ * This structure is used to represent the overall CCX file, including the header information and an array of data entries.
+ *
+ * @var header Header of the CCX file.
+ * @var data Array of data entries.
+ * @var dataCount Number of data entries.
+ */
+typedef struct {
+    CCXFileHeader header;  ///< Header of the CCX file.
+    CCXFileData *data;     ///< Array of data entries.
+    uint32_t dataCount;    ///< Number of data entries.
+} CCXFile;
+
+/**
+ * @struct CCXContext
+ * @brief Context structure for managing CCX file operations.
+ *
+ * This structure is used to manage CCX file operations, including storing the file path and a pointer to the CCX file structure.
+ *
+ * @var ccxFile Pointer to the CCX file structure.
+ * @var filePath Path to the CCX file.
+ */
+typedef struct {
+    CCXFile *ccxFile;   ///< Pointer to the CCX file structure.
+    char *filePath;     ///< Path to the CCX file.
+} CCXContext;
+
+/**
+ * @brief Function to initialize a CCX context with the given file path.
+ *
+ * This function initializes a CCX context by allocating memory and setting the file path.
+ *
+ * @param filePath Path to the CCX file.
+ * @return A pointer to the initialized CCX context.
+ */
+CCX_EXPORT CCXContext* CCXInitContext(const char *filePath);
+
+/**
+ * @brief Function to read and parse the CCX file into the context.
+ *
+ * This function reads the CCX file from the given file path and parses its content into the provided context.
+ *
+ * @param context Pointer to the CCX context.
+ */
+CCX_EXPORT CCXenum CCXReadFile(CCXContext *context);
+
+/**
+ * @brief Searches for files by name within a CCX context.
+ *
+ * This function traverses the CCX context to find files whose names contain the specified substring.
+ * It populates the provided `result` array with the matching CCXFileData entries and returns the
+ * number of matches found.
+ *
+ * @param context Pointer to the CCX context that contains the file data.
+ * @param filePath Substring to search for within the file names.
+ * @param result Array to store the matching CCXFileData entries.
+ * @return The number of matching CCXFileData entries found.
+ */
+CCX_EXPORT uint32_t CCXFindFilesByName(CCXContext *context, const char *filePath, CCXFileData **result);
+
+/**
+ * @brief Function to return an array of all file data entries within the CCX file.
+ *
+ * This function returns an array of all file data entries present in the CCX file.
+ *
+ * @param context Pointer to the CCX context.
+ * @return An array of pointers to all CCXFileData entries.
+ */
+CCX_EXPORT CCXFileData** CCXGetFiles(CCXContext *context);
+
+/**
+ * @brief Function to free the allocated memory in the CCX context.
+ *
+ * This function frees all allocated memory associated with the CCX context, including the CCX file structure and its data entries.
+ *
+ * @param context Pointer to the CCX context to be freed.
+ */
+CCX_EXPORT CCXenum CCXFreeContext(CCXContext *context);
+
+#endif // CCX_H
